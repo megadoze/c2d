adc7be527d918d8886769918284b78eaf53998a2
Oleg <Oleg@12f0f72f-67f8-4562-8a61-7b1493174f8c.fritz.box>
2024-07-08 15:53:58 +0200

    init

--- src/app.js
+++ src/app.js
@@ -1,3 +1,31 @@
+// Отслеживаем пересечение блока границ шапки сайта
+const options = {
+  root: document.querySelector(".body"),
+  rootMargin: "-156px 0px 0px 0px",
+  threshold: 0,
+};
+
+const callback = function (entries, observer) {
+  const header = document.querySelector(".header");
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      entry.target.classList.add("cross");
+      header.classList.add("animation");
+      console.log("Пересек!");
+      return;
+    } else {
+      entry.target.classList.remove("cross");
+      header.classList.remove("animation");
+      console.log("не пересек");
+    }
+  });
+};
+
+const Observer = new IntersectionObserver(callback, options);
+const target = document.querySelector(".booking-form__title");
+
+Observer.observe(target);
+
 // Ждем пока закрузится вся страница
 document.addEventListener("DOMContentLoaded", () => {
   // Menu
@@ -21,32 +49,4 @@ document.addEventListener("DOMContentLoaded", () => {
       body.style.height = "100vh";
     }
   };
-
-  // Отслеживаем пересечение блока границ шапки сайта
-  const options = {
-    root: document.querySelector(".body"),
-    rootMargin: "-156px 0px 0px 0px",
-    threshold: 0,
-  };
-
-  const callback = function (entries, observer) {
-    const header = document.querySelector(".header");
-    entries.forEach((entry) => {
-      if (!entry.isIntersecting) {
-        entry.target.classList.add("cross");
-        header.classList.add("animation");
-        console.log("Пересек!");
-        return;
-      } else {
-        entry.target.classList.remove("cross");
-        header.classList.remove("animation");
-        console.log("не пересек");
-      }
-    });
-  };
-
-  const Observer = new IntersectionObserver(callback, options);
-  const target = document.querySelector(".booking-form__title");
-
-  Observer.observe(target);
 });
